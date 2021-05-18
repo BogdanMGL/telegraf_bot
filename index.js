158c02ffba3222753b366fa56e3516fca3a9c018
BogdanMGL <sviridonov.bogdan.2014@gmail.com>
2021-05-18 16:57:07 +0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Telegraf} = require("telegraf");
-const bot = new Telegraf('1711139509:AAF0DRyquFVQ2ZRtNlohS9eEWVPyEbpuNL4');
+require('dotenv').config();
+const telegramToken = process.env.TOKEN ;
+const bot = new Telegraf(telegramToken);
 bot.start(ctx =>ctx.reply( `Hello ${ctx.update.message.from.first_name}`));
 bot.on('text',(ctx)=>{
     ctx.reply(`${ ctx.update.message.text}`);
